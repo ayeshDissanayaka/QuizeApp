fa7358e193d672f36b0697a808ba44d01fa920ba
Ayesh Dissanayaka <dilakshandissanayaka@gmail.com>
2020-04-28 17:27:48 +0530

    OriginalCommit

---
+++ lib/Questions.dart
@@ -0,0 +1,11 @@
+class Questions{
+  String QuestionText;
+  bool QuestionsAns;
+
+  Questions(String a, bool b){
+    QuestionText=a;
+    QuestionsAns=b;
+  }
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'QuizeBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain qBrain=QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> answerCheck =[];
+
+  void checkAnswer(bool userCheckAnswer){
+    bool correctAnswer=qBrain.getAnswerAsQues();
+    setState(() {
+
+
+      if (qBrain.isFinished() == true) {
+
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        qBrain.reset();
+        answerCheck = [];
+    }else{
+        if (userCheckAnswer == correctAnswer) {
+          answerCheck.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          answerCheck.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        qBrain.nextQuestion();
+      }
+    });
+  }
+  @override
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                qBrain.getQuestionAsNumber(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: answerCheck,
+        ),
+      ],
+    );
+  }
+}
